8d1c468588d13616655d37a3b4fd0f33bcd63c9b
BjoernWagner <bjoern.m.wagner@gmail.com>
2016-03-02 19:13:01 +0100

    Associated restaurant with reviews in the view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,9 +8,10 @@ class ReviewsController < ApplicationController
 	def create
 		@restaurant = Restaurant.find(params[:restaurant_id])
 		@restaurant.reviews.create(review_params)
+		redirect_to restaurants_path
 	end
 
-	def reviews_params
+	def review_params
 		params.require(:review).permit(:thoughts, :rating)
 	end
 
